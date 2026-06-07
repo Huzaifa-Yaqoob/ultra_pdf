66d2a39c9acc90d85b6463cc8f0234895e1a0423
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2026-06-07 15:51:05 +0500

    added central fonts and assets

--- apps/showcase/app/page.tsx
+++ apps/showcase/app/page.tsx
@@ -2,7 +2,8 @@ import { Button } from '@ultra-pdf/ui/components/ui/button';
 
 export default function Home() {
   return (
-    <div className={'bg-primary'}>
+    <div className={'font-heading text-xl'}>
+      Hello it is me
       <Button variant="outline">CLick me</Button>
     </div>
   );
